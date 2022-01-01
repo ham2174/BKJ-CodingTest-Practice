230bfec944f25c76573a24ab28d71be2d1d4f57e
GunHyung Ham <gdreagon@naver.com>
2022-01-02 00:03:44 +0900

    solve #1 : 1000

---
+++ N11022.java
@@ -0,0 +1,28 @@
+package IO;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class N11022 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+		
+		int count=1;
+		int T = Integer.parseInt(br.readLine());
+		
+		for(int i=0; i<T; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			sb.append("Case #" + count + ": ").append(a + " + " + b + " = ").append(a+b).append("\n");
+			count++;
+		}
+		
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
+	}
+}
