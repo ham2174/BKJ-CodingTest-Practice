9681669e5650a5ec6d132c301894b0b5bd159494
GunHyung Ham <gdreagon@naver.com>
2022-01-05 22:17:08 +0900

    solve #1 : 2440

---
+++ N2440.java
@@ -0,0 +1,28 @@
+package IO;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class N2440 {
+	
+		public static void main(String[] args) throws IOException {
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+			
+			int N = Integer.parseInt(br.readLine());
+			
+			for(int i=N; i>=0; i--) {
+				for(int j=0; j<i; j++) {
+					bw.write("*");
+				}
+				bw.write("\n");
+			}
+			
+			bw.flush();
+			bw.close();
+		}
+
+	}
