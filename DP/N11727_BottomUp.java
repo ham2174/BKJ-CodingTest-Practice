c1bb90144ac10dacee9a4b397b03f36947b27d3e
GunHyung Ham <gdreagon@naver.com>
2022-01-09 20:47:36 +0900

    solve #2 : 11727

---
+++ DP/N11727_BottomUp.java
@@ -0,0 +1,30 @@
+package DP;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class N11727_BottomUp { // ButtomUp
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int n = Integer.parseInt(br.readLine());
+        int[] dp = new int[n+1];
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 3;
+        
+        for(int i=3; i<=n; i++) {
+        	dp[i] = (dp[i-1] + 2 * dp[i-2]) % 10007;
+        }
+        
+        bw.write(String.valueOf(dp[n]%10007));
+        bw.flush();
+        bw.close();
+	}
+
+}
